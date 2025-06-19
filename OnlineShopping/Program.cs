3e946bbabf7ad3d3b2033b9a65cbf32f88b28162
Yujan Thulung Rai <yujanrai07@gmail.com>
2025-06-19 07:30:30 +0545

    Product dashboard

--- OnlineShopping/Program.cs
+++ OnlineShopping/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Product}/{action=ProductDashboard}/{id?}");
 
 app.Run();
